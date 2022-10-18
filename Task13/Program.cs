92df046d74568344447c913bc09d6a6f28de8de7
Michkasov Roman <mihjob@list.ru>
2022-10-19 01:17:16 +0300

    Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 13: Напишите программу, 
+//которая выводит третью цифру (справа налево) заданного числа 
+//или сообщает, что третьей цифры нет.
+
+Console.Clear();
+Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number / 100 == 0)
+{
+    Console.Write($"У числа {number} нет третьей цифры.");
+}
+
+else 
+{
+    while (Math.Abs(number) >= 1000) 
+    {
+        number = Math.Abs(number)/10;
+    }
+    int result = number % 10;
+    Console.Write(result);
+}
+
+
+
